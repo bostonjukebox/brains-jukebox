81b0db453297e1d0a5e95fff28a20f2447a9b102
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-10-01 18:16:55 -0400

    Add temporary fix to load images dynamically

--- src/components/SongImage.js
+++ src/components/SongImage.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import marty from '../img/image2.jpg'
 import covey from '../img/image3.jpg'
 
-const SongImage = ({song}) =>
-    <section>
-        <img src={song.img} alt={`${song.title} from ${song.author}`} />
-    </section>
+class SongImage extends Component {
+    render(){
+        const { song } = this.props;
+        const source = require(`../img/${song.img}`);
+
+        return (
+        <section>
+            <img src={source} alt={`${song.title} from ${song.author}`} />
+        </section>
+        )
+    }
+}
 
 SongImage.propTypes = {
     song: PropTypes.object.isRequired,
