e26ac0df84b2ed10980f7b84465905dc8908e5b4
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-10-01 17:29:26 -0400

    Add path to images

--- src/components/SongImage.js
+++ src/components/SongImage.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import marty from '../img/image2.jpg'
+import covey from '../img/image3.jpg'
+
 const SongImage = ({song}) =>
     <section>
-        <img alt={`${song.title} from ${song.author}`} />
+        <img src={song.img} alt={`${song.title} from ${song.author}`} />
     </section>
 
 SongImage.propTypes = {
     song: PropTypes.object.isRequired,
+    source: PropTypes.string.isRequired
 }    
 
 export default SongImage
