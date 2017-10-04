5c3ac4fca863112a0d7ab07729097e97631ca90c
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-10-04 00:14:56 -0400

    Add styles to multitrack.js:

--- src/components/MultiTrack.js
+++ src/components/MultiTrack.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import styled from 'styled-components'
+
 import Track from './Track'
 
 import vocals from "../img/icon-vocals.svg"
 import guitar from "../img/icon-guitar.svg"
 import drums from "../img/icon-drums.svg"
 
+const Container = styled.section`
+    margin-top: 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
 const MultiTrack = ({playing}) =>
-        <section>
+        <Container>
             <Track name={'vocal'} icon={vocals} playing={playing} />
             <Track name={'guitar'} icon={guitar} playing={playing}/>
             <Track name={'drums'} icon={drums} playing={playing}/>
-        </section>
+        </Container>
 
 MultiTrack.propTypes = {
     playing: PropTypes.bool,
--- src/components/Track.js
+++ src/components/Track.js
@@ -3,21 +3,39 @@ import PropTypes from 'prop-types'
 
 import styled from 'styled-components'
 
+const Container = styled.section`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: .31em;
+`
 const Icon = styled.img`
     width: 30px;
     height: 30px;
 `
+const Button = styled.button`
+    appearance: none;
+    background-color: #000;
+    color: #fff;
+    border: 0;
+    border-radius: 5px;
+    outline: 0;
+    font-size: .8rem;
+    padding: .6em;
+    cursor: pointer;
+`
 
 const Track = ({name, icon, playing}) =>
-    <section>
+    <Container>
         <Icon src={icon} alt={`${name} - icon`}/> 
         {name}
-        {(playing) ?
-            <button>Stop</button>
+        {(playing === false) ?
+            <Button>Stop</Button>
             : ''    
         }
         
-    </section>
+    </Container>
 
     Track.propTypes = {
         name: PropTypes.string,
