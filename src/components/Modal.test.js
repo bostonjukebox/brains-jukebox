2bb8050f62bd1a22b4eb7e23288e4429beea513d
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-10-07 00:20:03 -0400

    Add testing file for modals

---
+++ src/components/Modal.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Modal from './Modal'
+
+test('renders ok', () => {
+    const modal = shallow(<Modal />);
+    expect(modal).toMatchSnapshot();
+})
