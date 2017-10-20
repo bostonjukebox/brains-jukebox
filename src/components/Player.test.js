ebc6695ad7a99e99d3910241bd43b08b27b363c7
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-10-19 23:15:48 -0400

    Add test to check if songs components render

--- src/components/Player.test.js
+++ src/components/Player.test.js
@@ -11,4 +11,14 @@ describe('Player', () => {
     const component = shallow(<Player />)
     expect(component.state().numberOfSongs).toBeGreaterThan(0)
   })
+  it('renders <SongImage /> when a song is playing', () => {
+    const component = shallow(<Player />)
+    component.setState({ index: 1 })
+    expect(component.find('SongImage')).toHaveLength(1)
+  })
+  it('renders <SongInfo /> when a song is playing', () => {
+    const component = shallow(<Player />)
+    component.setState({ index: 1 })
+    expect(component.find('SongInfo')).toHaveLength(1)
+  })
 })
