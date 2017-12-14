0a6971980dd43c9e50d3664d6736cb100cfb0bdd
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-12-14 00:50:45 -0500

    Organize test files

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,38 +4,6 @@ import styled from 'styled-components'
 import Modal from './Modal'
 import ModalForm from './ModalForm'
 
-const Container = styled.section`
-    width: 100%;
-    height: 100%;
-`
-const Navigation = styled.nav`
-    position: absolute;
-    bottom: 5%;
-    right: 5%;
-    width: 25%;
-    display: flex;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-`
-
-const Item = styled.li`
-    list-style: none;
-`
-
-const Button = styled.button`
-    appearance: none;
-    border: none;
-    background-color: transparent;
-    color: #fff;
-    font-size: 1rem;
-    outline: none;
-    cursor: pointer;
-
-    &:hover {
-        color: #000;
-    }
-`
-
 class Menu extends Component {
   constructor() {
     super()
@@ -96,7 +64,7 @@ class Menu extends Component {
         {(this.state.showModalWorks) ?
           <Modal
             title="How it works"
-            text="The Boston TouchJukebox reads your mood and what you touch to offer a unique listening experience.\n\nWhen you wear the headband sensor, the Boston TouchJukebox starts to read your mood. According to how you feel, certain songs are available. \n\nFor example, if you're stressed and touch certain objects, the Boston TouchJukebox you recommend songs to keep you more relaxed. The interface of the TouchJukebox changes according to your mood. \n\nIf you notice that colors are changing, it is a signal that you're more or less concentrated on your experience. \n\nThe volume creates the same experience. If you get concentrated, the Boston TouchJukebox gets louder to provide you a more immersive experience with the song you're listening."
+            text="The Boston TouchJukebox reads your mood and what you touch to offer a unique listening experience. When you wear the headband sensor, the Boston TouchJukebox starts to read your mood. According to how you feel, certain songs are available. For example, if you're stressed and touch certain objects, the Boston TouchJukebox you recommend songs to keep you more relaxed. The interface of the TouchJukebox changes according to your mood.  If you notice that colors are changing, it is a signal that you're more or less concentrated on your experience. The volume creates the same experience. If you get concentrated, the Boston TouchJukebox gets louder to provide you a more immersive experience with the song you're listening."
             handleModal={this.toggleModalWorks}
           />
                 : ''
@@ -114,3 +82,35 @@ class Menu extends Component {
 }
 
 export default Menu
+
+
+const Container = styled.section`
+  width: 100%;
+  height: 100%;
+`
+const Navigation = styled.nav`
+  position: absolute;
+  bottom: 5%;
+  right: 5%;
+  width: 25%;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+`
+
+const Item = styled.li`
+  list-style: none;
+`
+const Button = styled.button`
+  appearance: none;
+  border: none;
+  background-color: transparent;
+  color: #fff;
+  font-size: 1rem;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+      color: #000;
+  }
+`
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,51 +3,6 @@ import PropTypes from 'prop-types'
 
 import styled from 'styled-components'
 
-const Container = styled.section`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-    background: rgba(0, 0, 0, 0.6);    
-`
-const Content = styled.section`
-    position: absolute;
-    z-index: 10;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 30%;
-    min-height: 15em;
-    background-color: #fff;
-    color: #000;
-    padding: 0 1em;
-    border-radius: 10px;
-    box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.6);     
-`
-const Title = styled.h1`
-    color: #F44336;
-    font-size: 1.2rem;
-`
-const Text = styled.p`
-    font-size: 1rem;
-    line-height: 1.5;
-    text-align: left;
-    white-space: pre-wrap;
-`
-const Button = styled.button`
-    appearance: none;
-    background-color: transparent;
-    color: #F44336;
-    border: none;
-    outline: none;
-    font-size: 1rem;
-    position: absolute;
-    top: .6em;
-    right: .6em;
-    cursor: pointer;
-`
 const Modal = ({ title, text, handleModal }) =>
   <Container>
     <Content>
@@ -64,3 +19,49 @@ Modal.propTypes = {
 }
 
 export default Modal
+
+const Container = styled.section`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background: rgba(0, 0, 0, 0.6);    
+`
+const Content = styled.section`
+  position: absolute;
+  z-index: 10;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30%;
+  min-height: 15em;
+  background-color: #fff;
+  color: #000;
+  padding: 0 1em;
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.6);     
+`
+const Title = styled.h1`
+  color: #F44336;
+  font-size: 1.2rem;
+`
+const Text = styled.p`
+  font-size: 1rem;
+  line-height: 1.5;
+  text-align: left;
+  white-space: pre-wrap;
+`
+const Button = styled.button`
+  appearance: none;
+  background-color: transparent;
+  color: #F44336;
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  position: absolute;
+  top: .6em;
+  right: .6em;
+  cursor: pointer;
+`
